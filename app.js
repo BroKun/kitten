b83331e12d8852f08f0bb35d3baef13244c90789
unknown <>
2017-07-17 01:10:03 +0800

    裁剪

--- app.js
+++ app.js
@@ -1,55 +1,50 @@
-/**
- * 应用程序入口
- */
+'use strict';
 
 const fs = require('fs');
-const path = require('path')
+const path = require('path');
 const yaml = require('js-yaml');
 const log4js = require('log4js');
 
-//读取配置信息
-let config = null;
-console.log(process.env.NODE_ENV);
+// 读取配置信息
+let config = {
+  env: process.env.NODE_ENV || 'dev',
+  rootDir: __dirname,
+};
 try {
-  config = yaml.safeLoad(fs.readFileSync(`${global.rootDir}/config.yml`, 'utf8'));
+  config = yaml.safeLoad(fs.readFileSync(`config/${config.env}.yml`, 'utf8'));
 } catch (e) {
   config = null;
   console.error('Could not load config file, error was: ', e);
   process.exit(1);
 }
-if (!fs.existsSync(path.join(global.rootDir, 'log'))) {
+if (!fs.existsSync(path.join('log'))) {
   try {
-    fs.mkdirSync(path.join(global.rootDir, 'log'));
+    fs.mkdirSync(path.join('log'));
   } catch (e) {
     console.error('Could not set up log directory, error was: ', e);
     process.exit(1);
   }
 }
 
-log4js.configure(`${global.rootDir}/log.json`);
+log4js.configure(path.join('./log.json'));
 if (process.env.NODE_ENV === 'unit-test') {
   log4js.setGlobalLogLevel(log4js.levels.ERROR);
 }
 const log = log4js.getLogger('app');
 
-if (!fs.existsSync(path.join(global.rootDir, config.commonFilePath))) {
+if (!fs.existsSync(path.join(config.commonFilePath))) {
   try {
-    fs.mkdirSync(path.join(global.rootDir, config.commonFilePath));
+    fs.mkdirSync(path.join(config.commonFilePath));
   } catch (e) {
     console.error('Could not set up commonFilePath directory, error was: ', e);
     process.exit(1);
   }
 }
-log.info(`commonFilePath：${path.join(global.rootDir, config.commonFilePath)}`);
-
+log.info(`commonFilePath：${path.join(config.commonFilePath)}`);
+let server = null;
 if (config) {
-  config.env = config.env || {};
-  config.env.mode = process.env.NODE_ENV || config.env.mode || 'development';
-  global.config = config;//配置作为全局信息global.config
-  log.info(`Run Mode : ${global.config.env.mode}`);
-
-  koala = require('./src/index');//启动控制器
-
-  server = koala();
+  const kitten = require('./src/index');// 启动控制器
+  kitten.config = config;
+  server = kitten.start();
 }
 module.exports = server;
--- src/server.js
+++ src/server.js
@@ -1,26 +1,28 @@
+'use strict';
+
 const Koa = require('koa2');
-const session = require('koa-generic-session');
+// const session = require('koa-generic-session');
 // const RedisStore = require('koa-redis');
-const Redis = require('ioredis');
+// const Redis = require('ioredis');
 const bodyParser = require('koa-bodyparser');
-const router = require('./router');
+// const router = require('./midware/router');
 const staticServer = require('koa-static');
-const mount = require('koa-mount'); // koa-mount@1.x
-const koaJwt = require('koa-jwt');
-const jwt = require('./jwt');
-//const graphqlHTTP = require('koa-graphql');
+// const mount = require('koa-mount'); // koa-mount@1.x
+// const koaJwt = require('koa-jwt');
+// const jwt = require('./jwt');
+// const graphqlHTTP = require('koa-graphql');
 // const log = require('../common/log');
-const logger = require('./logger');
+// const logger = require('./logger');
 const getHttp = require('http').Server;
-const co = require('co');
-const views = require('koa-views');
-const baseInit = require('./baseInit');
-const apiHistory = require('./koa-api-history');
-const dynamicService = require('../common/dynamicAPI');
+// const co = require('co');
+// const views = require('koa-views');
+// const baseInit = require('./baseInit');
+// const apiHistory = require('./koa-api-history');
+// const dynamicService = require('../common/dynamicAPI');
 const log4js = require('koa-log4');
 const path = require('path');
 
-log4js.configure(`${global.rootDir}/log.json`);
+log4js.configure(path.join('./log.json'));
 const log = log4js.getLogger('server');
 let server = null;
 /**
@@ -29,25 +31,25 @@ let server = null;
 
 function onError(error) {
   if (error.syscall !== 'listen') {
-    throw error
+    throw error;
   }
 
   const bind = typeof port === 'string'
     ? `Pipe ${global.config.port}`
-    : `Port ${global.config.port}`
+    : `Port ${global.config.port}`;
 
   // handle specific listen errors with friendly messages
   switch (error.code) {
     case 'EACCES':
-      log.error(`${bind} requires elevated privileges`)
-      process.exit(1)
-      break
+      log.error(`${bind} requires elevated privileges`);
+      process.exit(1);
+      break;
     case 'EADDRINUSE':
-      log.error(`${bind} is already in use`)
-      process.exit(1)
-      break
+      log.error(`${bind} is already in use`);
+      process.exit(1);
+      break;
     default:
-      throw error
+      throw error;
   }
 }
 
@@ -56,57 +58,56 @@ function onError(error) {
  */
 
 function onListening() {
-  const addr = server.address()
+  const addr = server.address();
   const bind = typeof addr === 'string'
     ? `pipe ${addr}`
-    : `port ${addr.port}`
+    : `port ${addr.port}`;
   log.info(`Listening on : ${bind}`);
 }
 const app = new Koa();
-app.StartServer = function () {
-  const config = global.config;
+app.start = function() {
   server = getHttp(app.callback());
-  server.on('error', onError)
-  server.on('listening', onListening)
+  server.on('error', onError);
+  server.on('listening', onListening);
 
   app.keys = [
-    'secret',
-    'whosyourdaddy',
+    app.config.key,
+    app.config.secret,
   ];
 
-  //session加密
-  const sess = {
-    key: 'brokun',
-    secret: 'brokun',
-    cookie: { maxAge: 1000 * 60 * config.sessionAge, },
-    resave: false,
-    rolling: true,
-    saveUninitialized: false,
-  };
-  //开发环境配置
-  //开发模式下使用内存管理session信息
-  //其他模式下使用redis管理session信息
-  if (global.config.env.mode !== 'development') {
-    let redisClient = config.sessionStore;
-    if (config.sessionStore.redis) {
-      redisClient = new Redis(config.sessionStore.redis);
-    }
-    if (config.sessionStore.cluster) {
-      redisClient = new Redis.Cluster(config.sessionStore.cluster);
-    }
-    // sess.store = new RedisStore(Object.assign({ client: redisClient, }, config.sessionStore.option));
-  }
-  ///TODO 需要完善
-  global.redis = new Redis(config.sessionStore.redis);
-  if (global.NODE_ENV === 'product') {
-    app.use(log4js.koaLogger(log4js.getLogger('http'), { level: 'auto', }));
-  }
-  app.use(staticServer(config.viewPath, config.staticServer));
-  app.use(views(config.viewPath, {
-    map: {
-      ejs: 'ejs',
-    },
-  }));
+  // //session加密
+  // const sess = {
+  //   key: 'brokun',
+  //   secret: 'brokun',
+  //   cookie: { maxAge: 1000 * 60 * config.sessionAge, },
+  //   resave: false,
+  //   rolling: true,
+  //   saveUninitialized: false,
+  // };
+  // //开发环境配置
+  // //开发模式下使用内存管理session信息
+  // //其他模式下使用redis管理session信息
+  // if (global.config.env.mode !== 'development') {
+  //   let redisClient = config.sessionStore;
+  //   if (config.sessionStore.redis) {
+  //     redisClient = new Redis(config.sessionStore.redis);
+  //   }
+  //   if (config.sessionStore.cluster) {
+  //     redisClient = new Redis.Cluster(config.sessionStore.cluster);
+  //   }
+  //   // sess.store = new RedisStore(Object.assign({ client: redisClient, }, config.sessionStore.option));
+  // }
+  // ///TODO 需要完善
+  // global.redis = new Redis(config.sessionStore.redis);
+  // if (global.NODE_ENV === 'product') {
+  //   app.use(log4js.koaLogger(log4js.getLogger('http'), { level: 'auto', }));
+  // }
+  app.use(staticServer(app.config.viewPath, app.config.staticServer));
+  // app.use(views(config.viewPath, {
+  //   map: {
+  //     ejs: 'ejs',
+  //   },
+  // }));
   app.use(bodyParser({
     textLimit: '50mb',
     formLimit: '50mb',
@@ -118,43 +119,43 @@ app.StartServer = function () {
     },
   }));
 
-  if (process.env.NODE_ENV === 'test') {
-    app.use(apiHistory({
-      dirPath: path.join(global.rootDir, config.commonFilePath),
-      postman: true,
-    }).unless({
-      method: 'GET',
-      path: [
-        /^\/huoma/,
-        /^\/api\/latest\/user\/login/,
-        /^\/api\/[1-9]?\d.[1-9]?\d.[1-9]?\d\/user\/login/,
-        /^\/login/,
-      ],
-    }));
-  }
+  // if (process.env.NODE_ENV === 'test') {
+  //   app.use(apiHistory({
+  //     dirPath: path.join(global.rootDir, config.commonFilePath),
+  //     postman: true,
+  //   }).unless({
+  //     method: 'GET',
+  //     path: [
+  //       /^\/huoma/,
+  //       /^\/api\/latest\/user\/login/,
+  //       /^\/api\/[1-9]?\d.[1-9]?\d.[1-9]?\d\/user\/login/,
+  //       /^\/login/,
+  //     ],
+  //   }));
+  // }
 
   // app.use(session(sess));
-  app.use(baseInit);
-  app.use(logger(server));
+  // app.use(baseInit);
+  // app.use(logger(server));
   // app.use(onlineChecker);
-  app.use(koaJwt({
-    secret: config.secret,
-    key: 'user',
-  }).unless({
-    method: 'GET',
-    path: [
-      /^\/huoma/,
-      /^\/api\/latest\/user\/login/,
-      /^\/api\/[1-9]?\d.[1-9]?\d.[1-9]?\d\/user\/login/,
-      /^\/login/,
-    ],
-  }));
-  app.use(jwt({
-    key: 'user',
-    rolling: config.tokenRolling,
-  }));
-  app.use(router.middleware());
-  server.listen(config.port, '0.0.0.0');
+  // app.use(koaJwt({
+  //   secret: config.secret,
+  //   key: 'user',
+  // }).unless({
+  //   method: 'GET',
+  //   path: [
+  //     /^\/huoma/,
+  //     /^\/api\/latest\/user\/login/,
+  //     /^\/api\/[1-9]?\d.[1-9]?\d.[1-9]?\d\/user\/login/,
+  //     /^\/login/,
+  //   ],
+  // }));
+  // app.use(jwt({
+  //   key: 'user',
+  //   rolling: config.tokenRolling,
+  // }));
+  // app.use(router.middleware());
+  server.listen(app.config.port, '0.0.0.0');
   return server;
-}
+};
 module.exports = app;
